a8b0309ec06cc2806cf2c3e27142afcbaf076742
Monwar-fiery <Monika.Wardziak@gmail.com>
2026-06-08 14:46:46 +0200

    Update test_emotion_detection.py

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -1,30 +1,27 @@
 import unittest
-import json
 from EmotionDetection import emotion_detector
 
 class TestEmotionDetector(unittest.TestCase):
 
     def test_emotion_detector(self):
         # Test Case 1: Joy
-        res_1 = json.loads(emotion_detector("I am glad this happened"))
-        # Example extraction based on standard Watson NLP dict structure
-        # If your output structure varies, adjust the key parsing accordingly
+        res_1 = emotion_detector("I am glad this happened")
         self.assertEqual(res_1['emotionPredictions'][0]['dominant_emotion'], 'joy')
         
         # Test Case 2: Anger
-        res_2 = json.loads(emotion_detector("I am really mad about this"))
+        res_2 = emotion_detector("I am really mad about this")
         self.assertEqual(res_2['emotionPredictions'][0]['dominant_emotion'], 'anger')
         
         # Test Case 3: Disgust
-        res_3 = json.loads(emotion_detector("I feel disgusted just hearing about this"))
+        res_3 = emotion_detector("I feel disgusted just hearing about this")
         self.assertEqual(res_3['emotionPredictions'][0]['dominant_emotion'], 'disgust')
         
         # Test Case 4: Sadness
-        res_4 = json.loads(emotion_detector("I am so sad about this"))
+        res_4 = emotion_detector("I am so sad about this")
         self.assertEqual(res_4['emotionPredictions'][0]['dominant_emotion'], 'sadness')
         
         # Test Case 5: Fear
-        res_5 = json.loads(emotion_detector("I am really afraid that this will happen"))
+        res_5 = emotion_detector("I am really afraid that this will happen")
         self.assertEqual(res_5['emotionPredictions'][0]['dominant_emotion'], 'fear')
 
 if __name__ == '__main__':
